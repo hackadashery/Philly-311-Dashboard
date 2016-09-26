bbf96f4ef8b053ff15a08112843216aec518b606
Akim Akimov <zedmor@gmail.com>
2016-09-26 15:16:14 -0400

    Started working on Flash app

--- Data/download.py
+++ Data/download.py
@@ -3,7 +3,7 @@ import os
 
 url = 'https://data.phila.gov/api/views/4t9v-rppq/rows.csv?accessType=DOWNLOAD'
 
-os.chdir(os.path.dirname(sys.argv[0])
+os.chdir(os.path.dirname(sys.argv[0]))
 r = requests.get(url)
 with open("311_Requests.csv", "wb") as code:
     code.write(r.content)

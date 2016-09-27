5e91b548fd156cbfd4c320a2c41a92aa8966f78e
Akim Akimov <zedmor@gmail.com>
2016-09-27 17:21:33 -0400

    some notebook work

--- Data/download.py
+++ Data/download.py
@@ -5,7 +5,7 @@ import logging
 import pandas as pd
 import datetime
 import urllib
-import wget
+#import wget
 
 url = 'https://data.phila.gov/api/views/4t9v-rppq/rows.csv?accessType=DOWNLOAD'
 logging.basicConfig(level=logging.DEBUG)
@@ -13,16 +13,17 @@ logging.basicConfig(level=logging.DEBUG)
 query = ("https://data.phila.gov/resource/4t9v-rppq.json")
 tdlist = ['expected_datetime','requested_datetime','updated_datetime']
 
-#raw_data = pd.read_json(query, convert_dates= tdlist)
+raw_data = pd.read_json(query, convert_dates= tdlist)
+raw_data=raw_data.set_index(['service_request_id'])
 
 os.chdir(os.path.dirname(sys.argv[0]))
-logging.debug('WorkDir',os.getcwd())
-wget.download(url)
 
 
+raw_data.to_pickle('311_Requests.pickle')
+#raw_data.to_csv('311_Requests.csv')
 #raw_data.to_csv('311_Requests.csv')
 
-# r = requests.get(url)
-# with open("311_Requests.csv", "wb") as code:
-#     code.write(r.content)
+r = requests.get(url)
+with open("311_Requests.csv", "wb") as code:
+    code.write(r.content)
 logging.debug("Download completed")

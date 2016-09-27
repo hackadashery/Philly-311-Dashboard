967993e90de3d147fadebf2eb1adb887e428180c
Akim Akimov <zedmor@gmail.com>
2016-09-27 12:34:12 -0400

    changed download to JSON

--- Data/download.py
+++ Data/download.py
@@ -2,12 +2,23 @@ import requests
 import os
 import sys
 import logging
+import pandas as pd
+import datetime
+import urllib
 
-url = 'https://data.phila.gov/api/views/4t9v-rppq/rows.csv?accessType=DOWNLOAD'
+#url = 'https://data.phila.gov/api/views/4t9v-rppq/rows.csv?accessType=DOWNLOAD'
 logging.basicConfig(level=logging.DEBUG)
 
+query = ("https://data.phila.gov/resource/4t9v-rppq.json")
+tdlist = ['expected_datetime','requested_datetime','updated_datetime']
+
+raw_data = pd.read_json(query, convert_dates= tdlist)
+
 os.chdir(os.path.dirname(sys.argv[0]))
-r = requests.get(url)
-with open("311_Requests.csv", "wb") as code:
-    code.write(r.content)
+
+raw_data.to_csv('311_Requests.csv')
+
+# r = requests.get(url)
+# with open("311_Requests.csv", "wb") as code:
+#     code.write(r.content)
 logging.debug("Download completed")

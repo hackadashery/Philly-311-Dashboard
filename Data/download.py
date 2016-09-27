c4c99a5fe6b1c0427a5019bc617c57264d4d556a
Akim Akimov <zedmor@gmail.com>
2016-09-27 12:08:47 -0400

    logging

--- Data/download.py
+++ Data/download.py
@@ -1,10 +1,13 @@
 import requests
 import os
 import sys
+import logging
 
 url = 'https://data.phila.gov/api/views/4t9v-rppq/rows.csv?accessType=DOWNLOAD'
+logging.basicConfig(level=logging.DEBUG)
 
 os.chdir(os.path.dirname(sys.argv[0]))
 r = requests.get(url)
 with open("311_Requests.csv", "wb") as code:
     code.write(r.content)
+logging.debug("Download completed")
